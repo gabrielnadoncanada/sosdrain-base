cf262527ee45238876008406cdfb78b595508bfc
Gabriel Nadon <gabrielnadoncanada@gmail.com>
2024-09-09 14:07:00 -0400

    Enhance PHPStorm configs and remove custom walker in header
    Add configuration options for MessDetector, PHPCSFixer, PHPCodeSniffer, PhpStan, and Psalm in `.idea/php.xml`. Remove the custom navigation menu walker from the Blade template in `header.blade.php`.

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -11,7 +11,6 @@
             'menu_class' => 'header_navbar-nav_list',
             'container_class' => 'header_navbar-nav',
             'container' => 'nav',
-            'walker' => new Custom_Nav_Menu_Walker()
         ]) ?>
       <button class="hamburger">
         <span class="line line--short"></span>
